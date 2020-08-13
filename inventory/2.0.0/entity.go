31e0c3babdd4ac95c7f59e38b29679906c99a2dd
Brian Shepanek <brianshepanek@gmail.com>
2020-08-13 10:04:14 -0400

    Update Inventory Entities

---
+++ inventory/2.0.0/entity.go
@@ -0,0 +1,83 @@
+package service
+
+type FobId string
+
+type FobPostalCode string
+
+type DimensionUom string
+
+type WeightUom string
+
+type PackageType string
+
+type Description string
+
+type ProductId string
+
+type PartId string
+
+type GroupName string
+
+type WsVersion string
+
+type Id string
+
+type Password string
+
+type LocalizationCountry string
+
+type LocalizationLanguage string
+
+type ColorName string
+
+type ProductName string
+
+type ProductBrand string
+
+type PrimaryImageUrl string
+
+type CountryOfOrigin string
+
+type PrimaryMaterial string
+
+type Shape string
+
+type Unspsc string
+
+type Gtin string
+
+type CautionComment string
+
+type NmfcDescription string
+
+type NmfcNumber string
+
+type LineName string
+
+type DefaultSetupCharge string
+
+type DefaultRunCharge string
+
+type ImprintSize string
+
+type CulturePoint string
+
+type LabelSize string
+
+type PartColor string
+
+type MainPart string
+
+type PartDescription string
+
+type ManufacturedItem string
+
+type BuyToOrder string
+
+type ReplenishmentLeadTime string
+
+type AttributeSelection string
+
+type InventoryLocationId string
+
+type InventoryLocationName string
