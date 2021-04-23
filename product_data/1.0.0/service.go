7c07127e78a47ad97575863d641c3e13a7699188
Brian Shepanek <brianshepanek@gmail.com>
2021-04-23 05:58:23 -0400

    Update shared object

--- product_data/1.0.0/service.go
+++ product_data/1.0.0/service.go
@@ -735,11 +735,11 @@ type SizeApparelArray struct {
 type GetProductRequest struct {
 	XMLName xml.Name `xml:"http://www.promostandards.org/WSDL/ProductDataService/1.0.0/ GetProductRequest" bson:"-"`
 
-	WsVersion *string `xml:"wsVersion,omitempty" json:"wsVersion,omitempty"`
+	WsVersion *string `xml:"http://www.promostandards.org/WSDL/ProductDataService/1.0.0/SharedObjects/ wsVersion,omitempty" json:"wsVersion,omitempty"`
 
-	Id *string `xml:"id,omitempty" json:"id,omitempty"`
+	Id *string `xml:"http://www.promostandards.org/WSDL/ProductDataService/1.0.0/SharedObjects/ id,omitempty" json:"id,omitempty"`
 
-	Password *string `xml:"password,omitempty" json:"password,omitempty"`
+	Password *string `xml:"http://www.promostandards.org/WSDL/ProductDataService/1.0.0/SharedObjects/ password,omitempty" json:"password,omitempty"`
 
 	LocalizationCountry *string `xml:"localizationCountry,omitempty" json:"localizationCountry,omitempty"`
 
