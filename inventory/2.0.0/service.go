4657f77eedc71925ab57b412c52fc1e05858ae62
Brian Shepanek <brianshepanek@gmail.com>
2024-01-10 11:35:46 -0500

    asd

--- inventory/2.0.0/service.go
+++ inventory/2.0.0/service.go
@@ -811,9 +811,9 @@ type GetInventoryLevelsRequest struct {
 type GetInventoryLevelsResponse struct {
 	XMLName xml.Name `xml:"http://www.promostandards.org/WSDL/Inventory/2.0.0/ GetInventoryLevelsResponse" bson:"-"`
 
-	Inventory *Inventory `xml:"http://www.promostandards.org/WSDL/Inventory/2.0.0/SharedObjects/ Inventory,omitempty" json:"Inventory,omitempty" bson:"inventory,omitempty"`
+	Inventory *Inventory `xml:"Inventory,omitempty" json:"Inventory,omitempty" bson:"inventory,omitempty"`
 
-	ServiceMessageArray *ServiceMessageArray `xml:"http://www.promostandards.org/WSDL/Inventory/2.0.0/SharedObjects/ ServiceMessageArray,omitempty" json:"ServiceMessageArray,omitempty" bson:"service_message_array,omitempty"`
+	ServiceMessageArray *ServiceMessageArray `xml:"ServiceMessageArray,omitempty" json:"ServiceMessageArray,omitempty" bson:"service_message_array,omitempty"`
 }
 
 type InventoryService interface {
