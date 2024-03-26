13912c80ab5c1ceb3c34fd43eadc81244d114611
Brian Shepanek <brianshepanek@gmail.com>
2024-03-26 08:53:00 -0400

    Update Media Content A

---
+++ media_content/1.1.0a/entity.go
@@ -0,0 +1,63 @@
+package service
+
+type FobId string
+
+type FobPostalCode string
+
+type DimensionUom string
+
+type WeightUom string
+
+type PackageType string
+
+type Description string
+
+type ProductId string
+
+type PartId string
+
+type GroupName string
+
+type WsVersion string
+
+type Id string
+
+type Password string
+
+type LocalizationCountry string
+
+type LocalizationLanguage string
+
+type ColorName string
+
+type ProductName string
+
+type ProductBrand string
+
+type PrimaryImageUrl string
+
+type CountryOfOrigin string
+
+type PrimaryMaterial string
+
+type Shape string
+
+type Unspsc string
+
+type Gtin string
+
+type CautionComment string
+
+type NmfcDescription string
+
+type NmfcNumber string
+
+type LineName string
+
+type DefaultSetupCharge string
+
+type DefaultRunCharge string
+
+type ImprintSize string
+
+type CulturePoint string
